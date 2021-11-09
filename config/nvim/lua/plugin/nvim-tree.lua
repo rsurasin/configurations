67363757ce7cd6e6f3ca3f333450e96933b36131
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2021-11-08 21:46:23 -0500

    Updated config to remove deprecated globals and moved to new setup function. Furthermore, updated the keymappings to new layout supported by the plugin.

--- config/nvim/lua/plugin/nvim-tree.lua
+++ config/nvim/lua/plugin/nvim-tree.lua
@@ -1,36 +1,47 @@
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_bindings = {
-    -- default mappings
-    ["<CR>"]           = tree_cb("edit"),
-    ["o"]              = tree_cb("edit"),
-    ["<2-LeftMouse>"]  = tree_cb("edit"),
-    ["<2-RightMouse>"] = tree_cb("cd"),
-    ["<C-]>"]          = tree_cb("cd"),
-    ["C-v"]            = tree_cb("vsplit"),
-    ["C-x"]            = tree_cb("split"),
-    ["<C-t>"]          = tree_cb("tabnew"),
-    ["<"]              = tree_cb("prev_sibling"),
-    [">"]              = tree_cb("next_sibling"),
-    ["<BS>"]           = tree_cb("close_node"),
-    ["<S-CR>"]         = tree_cb("close_node"),
-    ["<Tab>"]          = tree_cb("preview"),
-    ["I"]              = tree_cb("toggle_ignored"),
-    ["H"]              = tree_cb("toggle_dotfiles"),
-    ["R"]              = tree_cb("refresh"),
-    ["a"]              = tree_cb("create"),
-    ["d"]              = tree_cb("remove"),
-    ["r"]              = tree_cb("rename"),
-    ["<C-r>"]          = tree_cb("full_rename"),
-    ["<x>"]            = tree_cb("cut"),
-    ["c"]              = tree_cb("copy"),
-    ["p"]              = tree_cb("paste"),
-    ["y"]              = tree_cb("copy_name"),
-    ["Y"]              = tree_cb("copy_path"),
-    ["gy"]             = tree_cb("copy_absolute_path"),
-    ["[c"]             = tree_cb("prev_git_item"),
-    ["]c"]             = tree_cb("next_git_item"),
-    ["-"]              = tree_cb("dir_up"),
-    ["q"]              = tree_cb("close"),
+
+vim.g.nvim_tree_indent_markers = 1 -- Show indent markers when folders are open
+vim.g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 0
+}
+
+-- Default Mappings
+local list = {
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+  { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
+  { key = "<C-v>",                        cb = tree_cb("vsplit") },
+  { key = "<C-x>",                        cb = tree_cb("split") },
+  { key = "<C-t>",                        cb = tree_cb("tabnew") },
+  { key = "<",                            cb = tree_cb("prev_sibling") },
+  { key = ">",                            cb = tree_cb("next_sibling") },
+  { key = "P",                            cb = tree_cb("parent_node") },
+  { key = "<BS>",                         cb = tree_cb("close_node") },
+  { key = "<S-CR>",                       cb = tree_cb("close_node") },
+  { key = "<Tab>",                        cb = tree_cb("preview") },
+  { key = "K",                            cb = tree_cb("first_sibling") },
+  { key = "J",                            cb = tree_cb("last_sibling") },
+  { key = "I",                            cb = tree_cb("toggle_ignored") },
+  { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+  { key = "R",                            cb = tree_cb("refresh") },
+  { key = "a",                            cb = tree_cb("create") },
+  { key = "d",                            cb = tree_cb("remove") },
+  { key = "r",                            cb = tree_cb("rename") },
+  { key = "<C-r>",                        cb = tree_cb("full_rename") },
+  { key = "x",                            cb = tree_cb("cut") },
+  { key = "c",                            cb = tree_cb("copy") },
+  { key = "p",                            cb = tree_cb("paste") },
+  { key = "y",                            cb = tree_cb("copy_name") },
+  { key = "Y",                            cb = tree_cb("copy_path") },
+  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+  { key = "[c",                           cb = tree_cb("prev_git_item") },
+  { key = "]c",                           cb = tree_cb("next_git_item") },
+  { key = "-",                            cb = tree_cb("dir_up") },
+  { key = "s",                            cb = tree_cb("system_open") },
+  { key = "q",                            cb = tree_cb("close") },
+  { key = "g?",                           cb = tree_cb("toggle_help") },
 }
 
   vim.g.nvim_tree_icons = {
@@ -63,3 +74,50 @@ vim.g.nvim_tree_bindings = {
     },
   }
 
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = true, -- Closes the tree when its the last window
+  open_on_tab         = true, -- Will open the tree (if open) when switching to a new tab
+  hijack_cursor       = false,
+  update_cwd          = false,
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  }
+}
