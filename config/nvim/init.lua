a14cb5599c7bd82a99297df6b39ef7b3b166cbdc
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2021-11-12 02:28:45 -0500

    Converted from vimscript to lua.

---
+++ config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Fish doesn't play well with others
+vim.o.shell = "/bin/bash"
+
+-- Leader key -> <Space>
+-- Set leader key early in config
+vim.g.mapleader = " "
+
+-- Packer Plugins
+require('impatient')
+require('plugins')
+
+-- Helper Script
+require('scripts')
