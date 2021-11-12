e7cf7cd7bdaad73479ad168c9521f8bb874a5a20
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2021-11-11 20:32:16 -0500

    Updated lsp config. Removed code to support nvim-lspinstall because using new plugin called nvim-lsp-installer

--- config/nvim/lua/plugin/nvim-lspconfig.lua
+++ config/nvim/lua/plugin/nvim-lspconfig.lua
@@ -21,7 +21,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', 'gT', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
@@ -33,10 +33,12 @@ local on_attach = function(client, bufnr)
 
 end
 
+-- DEPRECATED: From https://github.com/kabouzeid/nvim-lspinstall
+-- Replaced with https://github.com/williamboman/nvim-lsp-installer
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "pyright", "rust_analyzer", "tsserver" }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
-end
+-- local servers = { "pyright", "rust_analyzer", "tsserver", "dockerls", "cssls", "sumneko_lua", "yamlls" }
+-- for _, lsp in ipairs(servers) do
+--   nvim_lsp[lsp].setup { on_attach = on_attach }
+-- end
 
