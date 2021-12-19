0ea815642f44e5623efe9c743548b928896b72cc
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2021-12-19 18:30:29 -0500

    refactor: use plenary to check whether macOS or Linux folder exists; if not check the bin folder. create path for sumneko_binary after the if statements to avoid redundant code.

--- config/nvim/lua/lsp/lua-ls.lua
+++ config/nvim/lua/lsp/lua-ls.lua
@@ -1,4 +1,6 @@
   -- Setup Lua Language Server
+local Path = require'plenary.path'
+
 local sumneko_root_path
 local sumneko_binary
 
@@ -6,11 +8,9 @@ local system_name
 if vim.fn.has("mac") == 1 then
   system_name = "macOS"
   sumneko_root_path = vim.fn.getenv 'HOME'..'/.local/share/nvim/lsp_servers/sumneko_lua/extension/server'
-  sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 elseif vim.fn.has("unix") == 1 then
   system_name = "Linux"
   sumneko_root_path = vim.fn.getenv 'HOME'..'/.local/share/nvim/lsp_servers/sumneko_lua/extension/server'
-  sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 else
   print("Unsupported system for sumneko")
 end
@@ -18,6 +18,11 @@ end
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
+
+sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+if not Path:new(sumneko_binary):exists() then
+    sumneko_binary = sumneko_root_path.."/bin/lua-language-server"
+end
 
 require'lspconfig'.sumneko_lua.setup {
   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
