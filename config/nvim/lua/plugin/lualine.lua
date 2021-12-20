04a44732bdef34f398ef541a563c669de3ae8d08
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2021-12-19 23:28:05 -0500

    fix: Neovim v0.6 deprecates nvim_lsp in favor for nvim_diagnostic, which uses Neovim's diagnostic API.

--- config/nvim/lua/plugin/lualine.lua
+++ config/nvim/lua/plugin/lualine.lua
@@ -10,7 +10,7 @@ require'lualine'.setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', {'diagnostics', sources = {'nvim_lsp'}}},
+    lualine_b = {'branch', {'diagnostics', sources = {'nvim_diagnostic'}}},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'filetype'},
     lualine_y = {'progress'},
