948f7c5fa46fe045ea3b6a9c3c54f0551d10c824
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2021-12-21 12:37:58 -0500

    feat: Added helpful keymaps: buffer window management, redo, nvim-tree toggle.

--- config/nvim/plugin/keymaps.lua
+++ config/nvim/plugin/keymaps.lua
@@ -5,9 +5,31 @@ vim.cmd [[runtime plugin/astronauta.vim]]
 local noremap = vim.keymap.noremap
 local nnoremap = vim.keymap.nnoremap
 
+-- Neovim
+nnoremap({ "U", "<C-r>" }) -- Redo
+-- Search and replace word under cursor
+nnoremap({ "<leader>*", "*``cgn" }) -- Replace going forwards
+nnoremap({ "<leader>#", "#``cgN" }) -- Replace going backwards
+-- Buffers
+nnoremap({ "<leader><Tab>", ":b#<CR>" }) -- Switch to last used buffer
+nnoremap({ "<leader>=", "<C-W>=" }) -- Buffer windows same size
+nnoremap({ "<leader>_", "<C-W>_" }) -- Buffer window take entire height
+nnoremap({ "<leader>|", "<C-W>|" }) -- Buffer window take entire width
+-- Prepend w/ a number to repeat command
+nnoremap({ "<leader>,", "<C-W>5<" }) -- Current buffer window resize smaller
+nnoremap({ "<leader>.", "<C-W>5>" }) -- Current buffer window resize larger
+-- Prepend w/ a number to repeat command
+nnoremap({ "<leader>-", "<C-W>5-" }) -- Current buffer window resize shorter
+nnoremap({ "<leader>+", "<C-W>5+" }) -- Current buffer window resize taller
+nnoremap({ "<leader>d", ":bd<CR>" }) -- Delete Buffer
+
+-- bufferline
+nnoremap({ "gb", ":BufferLinePick<CR>", silent = true }) -- Choose Buffer
+nnoremap({ "<leader>n", ":BufferLineCycleNext<CR>", silent = true }) -- Next Buffer
+nnoremap({ "<leader>p", ":BufferLineCyclePrev<CR>", silent = true }) -- Prev Buffer
+
 -- nvim-tree
-nnoremap({ "<leader>o", ":NvimTreeOpen<CR>" }) -- Open nvim-tree
-nnoremap({ "<leader>q", ":NvimTreeClose<CR>" }) -- Close nvim-tree 
+nnoremap({ "<leader>o", ":NvimTreeToggle<CR>" }) -- Open/Close nvim-tree
 
 -- navigator
 noremap({ "<C-h>", "<CMD>lua require('Navigator').left()<CR>", silent = true })
