1355e8308ba0a9395a3a3368e052cb06175afdbc
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2022-06-04 22:40:47 -0400

    feat: Add setup function for gitsigns w/ custom keymap.

--- config/nvim/lua/plugin/gitsigns.lua
+++ config/nvim/lua/plugin/gitsigns.lua
@@ -1,3 +1,41 @@
 -- https://github.com/lewis6991/gitsigns.nvim
-require('gitsigns').setup()
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
 
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+   map('n', '<leader>hn', function()
+     if vim.wo.diff then return ']c' end
+     vim.schedule(function() gs.next_hunk() end)
+     return '<Ignore>'
+   end, {expr=true})
+
+   map('n', '<leader>hN', function()
+     if vim.wo.diff then return '[c' end
+     vim.schedule(function() gs.prev_hunk() end)
+     return '<Ignore>'
+   end, {expr=true})
+
+    -- Actions
+   map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+   map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+   map('n', '<leader>hS', gs.stage_buffer)
+   map('n', '<leader>hu', gs.undo_stage_hunk)
+   map('n', '<leader>hR', gs.reset_buffer)
+   map('n', '<leader>hp', gs.preview_hunk)
+   map('n', '<leader>hB', function() gs.blame_line{full=true} end)
+   map('n', '<leader>ht', gs.toggle_current_line_blame)
+   map('n', '<leader>hd', gs.diffthis)
+   map('n', '<leader>hD', function() gs.diffthis('~') end)
+--   map('n', '<leader>td', gs.toggle_deleted)
+
+   -- Text object
+   map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+ end
+}
