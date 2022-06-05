afe9c7dcbb56caaff466bcf58b1725e9a2816cb2
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2022-06-04 22:49:03 -0400

    feat/refactor: Show lsp indicator and moved/added new keymap.
    Moved bufferline specific keymap from plugin/keymap to nvim-bufferline.lua.

--- config/nvim/lua/plugin/nvim-bufferline.lua
+++ config/nvim/lua/plugin/nvim-bufferline.lua
@@ -8,6 +8,15 @@ require("bufferline").setup {
                 highlight = "Directory",
                 text_align = "left"
             }
-        }
+        },
+        diagnostics = "nvim_lsp"
     }
 }
+
+-- Keymap
+vim.keymap.set("n", "gb", ":BufferLinePick<CR>", { silent = true }) -- Choose Buffer
+vim.keymap.set("n", "<leader>n", ":BufferLineCycleNext<CR>", { silent = true }) -- Next Buffer
+vim.keymap.set("n", "<leader>p", ":BufferLineCyclePrev<CR>", { silent = true }) -- Prev Buffer
+vim.keymap.set("n", "<M-n>", ":BufferLineMoveNext<CR>", { silent = true }) -- Next Buffer
+vim.keymap.set("n", "<M-p>", ":BufferLineMovePrev<CR>", { silent = true }) -- Prev Buffer
+vim.keymap.set("n", "<leader>bp", ":BufferLineTogglePin<CR>", { silent = true }) -- Prev Buffer
