d0dc742a6e5ac7a8a36d4efd02cc1a42057ea06a
Rahul Surasinghe <41207879+rsurasin@users.noreply.github.com>
2022-06-04 22:44:59 -0400

    feat/refactor: Move navigator specific keymap from plugin/keymap.lua to navigator.lua and update to use vim.keymap.set.

--- config/nvim/lua/plugin/navigator.lua
+++ config/nvim/lua/plugin/navigator.lua
@@ -3,3 +3,10 @@ require('Navigator').setup({
     auto_save = 'current',
     disable_on_zoom = true
 })
+
+-- Keymap
+vim.keymap.set("n", "<C-h>", "<CMD>lua require('Navigator').left()<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<CMD>lua require('Navigator').up()<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<CMD>lua require('Navigator').right()<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<CMD>lua require('Navigator').down()<CR>", { silent = true })
+vim.keymap.set("n", "<C-p>", "<CMD>lua require('Navigator').previous()<CR>", { silent = true })
